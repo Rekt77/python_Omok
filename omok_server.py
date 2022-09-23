c205dbabb3f095bca030f61f2b7f09d902123ce7
Rekt77 <timtaeil@gmail.com>
2022-09-24 00:02:36 +0900

    main_server 루틴완성

---
+++ omok_server.py
@@ -0,0 +1,34 @@
+import socket
+import pickle
+import os
+
+name = input("사용자명을 입력하세요 : ")
+host = input("서버 ip를 입력하세요 : ")
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+client.connect((host, 7777))
+client.sendall(name.encode())
+vector = [(1,0),(-1,0),(0,1),(0,-1),
+        (-1,-1),(-1,1),(1,-1),(1,1)]
+
+print(client.recv(4096).decode())
+board = pickle.loads(client.recv(4096))
+while True:
+    print(client.recv(4096).decode())
+    board = pickle.loads(client.recv(4096))
+    try:
+        x, y = map(int, input("(x, y) >>").split(","))
+        if board[x][y] == "□":
+            board[x][y] = "●"
+        else:
+            print("해당 위치에는 놓ㅎ을 수 없습니다.")
+            continue
+    except IndexError:
+        print("잘못된 위치입니다.")
+        continue
+    os.system("cls")
+
+    client.send(pickle.dumps(board))
+    print(client.recv(4096).decode())
+        
